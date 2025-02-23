8887633f763da57137724ca4b8bb9958f4f37b2d
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-02-24 00:52:23 +0900

    🐛 Fix : image optimization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,14 @@ import Image from "next/image";
 export default function RootPage() {
   const title = <div id="title">Fluent React</div>;
   const bookImage = (
-    <Image src="/front.jpeg" alt="book cover" fill objectFit="contain" />
+    <Image
+      src="/front.jpeg"
+      alt="book cover"
+      fill
+      priority
+      sizes="33vw"
+      style={{ objectFit: "contain" }}
+    />
   );
   return (
     <main>
