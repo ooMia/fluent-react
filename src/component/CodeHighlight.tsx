144f03fda186043c80d94ba4663dca6140bd46dd
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-02-24 00:37:57 +0900

    ✨ Feat : clipboard
    - to client component

--- src/component/CodeHighlight.tsx
+++ src/component/CodeHighlight.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
 import jsx from "react-syntax-highlighter/dist/esm/languages/prism/jsx";
 import prism from "react-syntax-highlighter/dist/esm/styles/prism/prism";
@@ -5,9 +7,41 @@ import prism from "react-syntax-highlighter/dist/esm/styles/prism/prism";
 SyntaxHighlighter.registerLanguage("jsx", jsx);
 
 export default function CodeHighlight({ children }: { children: string }) {
+  const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(children);
+      showTooltip();
+    } catch (error) {
+      console.error("Failed to copy text: ", error);
+    }
+  };
+
+  const showTooltip = () => {
+    const tooltip = document.getElementById("tooltip");
+    if (tooltip) {
+      tooltip.style.visibility = "visible";
+      setTimeout(() => {
+        tooltip.style.visibility = "hidden";
+      }, 1500);
+    }
+  };
+
   return (
-    <SyntaxHighlighter language="jsx" style={prism}>
-      {children}
-    </SyntaxHighlighter>
+    <div style={{ position: "relative" }}>
+      <SyntaxHighlighter language="jsx" style={prism}>
+        {children}
+      </SyntaxHighlighter>
+      <button
+        onClick={handleCopy}
+        style={{
+          position: "absolute",
+          top: 15,
+          right: 10,
+        }}
+      >
+        📋
+      </button>
+      <div id="tooltip">Copied!</div>
+    </div>
   );
 }
