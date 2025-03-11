f73c2a9cc928bef83ccff4eca06034bce4b3a78f
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-03-11 10:00:44 +0900

    ✨ Feat : chapter 5-1 React.memo

--- src/app/patterns/layout.tsx
+++ src/app/patterns/layout.tsx
@@ -4,20 +4,27 @@ export default async function Layout({
   children,
   memoAdd,
   memoAddFetch,
+  memoToDo,
 }: {
   children: React.ReactNode;
   memoAdd: React.ReactNode;
   memoAddFetch: React.ReactNode;
+  memoToDo: React.ReactNode;
 }) {
   return (
     <main>
       {children}
       <TabView>
         <>
-          <header>React.memo</header>
+          <header>
+            <i className="code-highlight code-highlight-special pulse m-1 mr-4">
+              React.memo
+            </i>
+          </header>
           <TabView>
             {memoAdd}
             {memoAddFetch}
+            {memoToDo}
           </TabView>
         </>
         <></>
