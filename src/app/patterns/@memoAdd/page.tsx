758ce4f22728def77e22be025ea6b3f3789907b6
Hyeon-hak Kim <hyeonhak.kim.dev@gmail.com>
2025-03-11 09:58:05 +0900

    🐛 Fix : minor styling for mobile

--- src/app/patterns/@memoAdd/page.tsx
+++ src/app/patterns/@memoAdd/page.tsx
@@ -1,14 +1,12 @@
+import CodeHighlight from "@/component/CodeHighlight";
 import { Component } from "./Example";
 
 export default async function MemoAdd() {
   return (
     <>
-      <header>
-        Try using
-        <i className="code-highlight code-highlight-special pulse m-1 mr-4">
-          React.memo
-        </i>
-        with pure functions
+      <header className="w-full justify-center">
+        <b>Pure Function</b>
+        <CodeHighlight>{`const add = (a, b) => a + b;`}</CodeHighlight>
       </header>
       <Component />
     </>
--- src/app/patterns/@memoAddFetch/page.tsx
+++ src/app/patterns/@memoAddFetch/page.tsx
@@ -1,12 +1,14 @@
+import CodeHighlight from "@/component/CodeHighlight";
 import { Component } from "./Example";
 export default async function MemoAddFetch() {
   return (
     <>
-      <header>
-        <i className="code-highlight code-highlight-special pulse m-1 mr-4">
-          React.memo
-        </i>
-        with side effects
+      <header className="w-full justify-center">
+        <b>Side Effect</b>
+        <CodeHighlight>
+          {`fetch(apiUrl)
+  .then((res) => res.json())`}
+        </CodeHighlight>
       </header>
       <Component />
     </>
